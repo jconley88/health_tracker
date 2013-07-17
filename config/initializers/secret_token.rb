13dc6f40549fb7ab04226fa2d000320e76d40eb4
Jon Conley <conley.jj@gmail.com>
2013-07-17 14:46:02 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthTracker::Application.config.secret_key_base = 'efe83960a03a51c500e98c5540080f7c5d0fbea8cf12b21d7431da0dce53262bab5f4982a67efb8fa6fc9acd35684091c5ef51ad50818d96a8378c10436e79ab'

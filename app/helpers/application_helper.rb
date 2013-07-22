f9088ead4b3153e074a0d8f52aaa38ef3a2c74b5
Jon Conley <conley.jj@gmail.com>
2013-07-22 14:52:28 -0600

    Zurbify flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def alert_class(flash_type)
+    flash_type = flash_type.to_s
+    case flash_type
+      when 'notice'; 'success'
+      else flash_type
+    end
+  end
 end
